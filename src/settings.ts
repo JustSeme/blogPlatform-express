2d689b0b57c181ffdb1e7eea01b772e752276e51
JustSemen <semyn03@mail.ru>
2023-02-13 13:39:20 +0300

    add settings for test vercel

---
+++ src/settings.ts
@@ -0,0 +1,11 @@
+const username = "justSeme"
+const password = "RMMXpX1hUlXqbKED"
+
+export const settings = {
+    mongoURI: process.env.mongoURI || `mongodb+srv://${username}:${password}@cluster86890.fgczccf.mongodb.net/?retryWrites=true&w=majority`,
+    JWT_SECRET: process.env.JWT_SECRET || 'super_secret',
+    PORT: process.env.PORT || 3000,
+    GMAIL_LOGIN: process.env.GMAIL_LOGIN || 'kepxep69@gmail.com',
+    GMAIL_APP_PASSWORD: process.env.GMAIL_APP_PASSWORD || 'gpllbohdhqcrdvnh',
+    test: 'test'
+}
