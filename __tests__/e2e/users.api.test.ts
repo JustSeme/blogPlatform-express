4314bb9b15a655ee05125ab5ca70ff2521c86ae1
JustSemen <semyn03@mail.ru>
2023-03-06 14:21:21 +0300

    made users api e2e test

--- __tests__/e2e/users.api.test.ts
+++ __tests__/e2e/users.api.test.ts
@@ -5,6 +5,8 @@ import { app } from '../../src/settings'
 
 const baseURL = '/homeworks/users'
 
+const generateEmail = (str: string) => `${str}@mail.ru`
+
 describe('/users', () => {
     beforeAll(async () => {
         await request(app)
@@ -28,9 +30,6 @@ describe('/users', () => {
             .post(`${baseURL}`)
             .send(correctUserInputData)
             .expect(HTTP_STATUSES.UNAUTHORIZED_401)
-
-        console.log(response.body);
-
     })
 
     it(`shouldn't create new user with incorrect input data`, async () => {
@@ -58,6 +57,7 @@ describe('/users', () => {
         })
     })
 
+    let createdUser: any
     it('should create new user with basic auth token', async () => {
         const response = await request(app)
             .post(`${baseURL}`)
@@ -66,13 +66,108 @@ describe('/users', () => {
             .expect(HTTP_STATUSES.CREATED_201)
 
         const { id, login, email, createdAt } = response.body
-        console.log(response.body);
+        createdUser = response.body
 
-        expect(id).not.toBe(null)
-        expect(login).not.toBe(null)
-        expect(email).not.toBe(null)
-        expect(createdAt).not.toBe(null)
+        expect(typeof id).toBe('string')
+        expect(typeof login).toBe('string')
+        expect(typeof email).toBe('string')
+        expect(typeof createdAt).toBe('string')
     })
 
+    it('should return array with one created user', async () => {
+        const response = await request(app)
+            .get(`${baseURL}`)
+            .set('Authorization', 'Basic YWRtaW46cXdlcnR5')
+            .expect(HTTP_STATUSES.OK_200)
 
+        expect(response.body.items.length === 1).toBe(true)
+        expect(response.body.items[0]).toEqual(createdUser)
+    })
+
+    let id1: string, id2: string, id3: string
+    it('should create three users and return error if email adress and login is already use', async () => {
+        const firstUser = {
+            login: 'abc',
+            password: '123123123',
+            email: generateEmail('JohnCena')
+        }
+        const secondUser = {
+            login: 'def',
+            password: '123123123',
+            email: generateEmail('JohnDoe')
+        }
+        const thirdUser = {
+            login: 'ghi',
+            password: '123123123',
+            email: generateEmail('justCena')
+        }
+
+        const response1 = await request(app)
+            .post(`${baseURL}`)
+            .set('Authorization', 'Basic YWRtaW46cXdlcnR5')
+            .send(firstUser)
+            .expect(HTTP_STATUSES.CREATED_201)
+        expect(typeof response1.body.id).toBe('string')
+        id1 = response1.body.id
+
+        const response2 = await request(app)
+            .post(`${baseURL}`)
+            .set('Authorization', 'Basic YWRtaW46cXdlcnR5')
+            .send(secondUser)
+            .expect(HTTP_STATUSES.CREATED_201)
+        expect(typeof response2.body.id).toBe('string')
+        id2 = response2.body.id
+
+        const response3 = await request(app)
+            .post(`${baseURL}`)
+            .set('Authorization', 'Basic YWRtaW46cXdlcnR5')
+            .send(thirdUser)
+            .expect(HTTP_STATUSES.CREATED_201)
+        expect(typeof response3.body.id).toBe('string')
+        id3 = response3.body.id
+
+        const response4 = await request(app)
+            .post(`${baseURL}`)
+            .set('Authorization', 'Basic YWRtaW46cXdlcnR5')
+            .send(secondUser)
+            .expect(HTTP_STATUSES.BAD_REQUEST_400)
+        expect(response4.body).toEqual({
+            errorsMessages: [
+                {
+                    "message": "Login already in use",
+                    "field": "login"
+                },
+                {
+                    "message": "Email already in use",
+                    "field": "email"
+                }
+            ]
+        })
+    })
+
+    it('should return array of users with asc sorting for login', async () => {
+        const response = await request(app)
+            .get(`${baseURL}?sortDirection=asc&sortBy=login`)
+            .set('Authorization', 'Basic YWRtaW46cXdlcnR5')
+            .expect(HTTP_STATUSES.OK_200)
+
+        expect(response.body.items.length === 4).toBe(true)
+
+        expect(response.body.items[0].id).toEqual(id1)
+        expect(response.body.items[1].id).toEqual(id2)
+        expect(response.body.items[2].id).toEqual(id3)
+        expect(response.body.items[3].id).toEqual(createdUser.id)
+    })
+
+    it('should return two users with email regex John', async () => {
+        const response = await request(app)
+            .get(`${baseURL}?searchEmailTerm=John&sortDirection=asc`)
+            .set('Authorization', 'Basic YWRtaW46cXdlcnR5')
+            .expect(HTTP_STATUSES.OK_200)
+
+        expect(response.body.items.length === 2).toBe(true)
+
+        expect(response.body.items[0].id).toBe(id1)
+        expect(response.body.items[1].id).toBe(id2)
+    })
 })
--- src/repositories/query/users-query-repository.ts
+++ src/repositories/query/users-query-repository.ts
@@ -1,4 +1,3 @@
-import { add } from "date-fns";
 import { ReadUsersQuery } from "../../models/users/ReadUsersQuery";
 import { UserDBModel } from "../../models/users/UserDBModel";
 import { UsersWithQueryOutputModel, UserViewModel } from "../../models/users/UsersViewModel";
@@ -9,31 +8,31 @@ export const usersQueryRepository = {
         const { sortDirection = 'desc', sortBy = 'createdAt', pageNumber = 1, pageSize = 10, searchLoginTerm = null, searchEmailTerm = null } = queryParams
 
         const filterArray: any = []
-        if(searchEmailTerm) {
-            filterArray.push({email: {$regex: searchEmailTerm, $options: 'i'}})
+        if (searchEmailTerm) {
+            filterArray.push({ email: { $regex: searchEmailTerm, $options: 'i' } })
         }
-        if(searchLoginTerm) {
-            filterArray.push({login: {$regex: searchLoginTerm, $options: 'i'}})
+        if (searchLoginTerm) {
+            filterArray.push({ login: { $regex: searchLoginTerm, $options: 'i' } })
         }
 
-        const filterObject = filterArray.length ? {$or: filterArray} : {}
+        const filterObject = filterArray.length ? { $or: filterArray } : {}
 
         const totalCount = await usersCollection.count(filterObject)
         const pagesCount = Math.ceil(totalCount / +pageSize)
-        
+
         const skipCount = (+pageNumber - 1) * +pageSize
-        let usersCursor = await usersCollection.find(filterObject, { projection: { _id: 0 }}).skip(skipCount).limit(+pageSize)
+        let usersCursor = await usersCollection.find(filterObject, { projection: { _id: 0 } }).skip(skipCount).limit(+pageSize)
 
         const sortDirectionNumber = sortDirection === 'asc' ? 1 : -1
-        const resultedUsers = await usersCursor.sort({[sortBy]: sortDirectionNumber}).toArray()
-        
+        const resultedUsers = await usersCursor.sort({ [sortBy]: sortDirectionNumber }).toArray()
+
         const displayedUsers: UserViewModel[] = resultedUsers.map(u => ({
             id: u.id,
             login: u.login,
             email: u.email,
             createdAt: u.createdAt
         }))
-        
+
         return {
             pagesCount: pagesCount,
             page: +pageNumber,
@@ -44,26 +43,26 @@ export const usersQueryRepository = {
     },
 
     async findUserById(userId: string) {
-        return await usersCollection.findOne({id: userId}, { projection: { _id: 0 }})
+        return await usersCollection.findOne({ id: userId }, { projection: { _id: 0 } })
     },
 
     async findUserByEmail(email: string) {
-        return await usersCollection.findOne({email: email})
+        return await usersCollection.findOne({ email: email })
     },
-    
+
     async findUserByLogin(login: string) {
-        return await usersCollection.findOne({login: login})
+        return await usersCollection.findOne({ login: login })
     },
 
     async findUserByLoginOrEmail(loginOrEmail: string): Promise<UserDBModel | null> {
-        return await usersCollection.findOne({$or: [{login: loginOrEmail}, {email: loginOrEmail}]})
+        return await usersCollection.findOne({ $or: [{ login: loginOrEmail }, { email: loginOrEmail }] })
     },
 
     async findUserByConfirmationCode(code: string) {
-        return await usersCollection.findOne({'emailConfirmation.confirmationCode': code})
+        return await usersCollection.findOne({ 'emailConfirmation.confirmationCode': code })
     },
 
     async findUserByRecoveryPasswordCode(code: string) {
-        return await usersCollection.findOne({'passwordRecovery.confirmationCode': code})
+        return await usersCollection.findOne({ 'passwordRecovery.confirmationCode': code })
     },
 }
