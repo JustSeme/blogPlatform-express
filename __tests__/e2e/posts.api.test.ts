3c9aa955640b7bc6d3f69ac3040d62eb4074e165
JustSemen <semyn03@mail.ru>
2023-03-21 13:59:15 +0300

    make posts api test positive invariants

--- __tests__/e2e/posts.api.test.ts
+++ __tests__/e2e/posts.api.test.ts
@@ -1,6 +1,7 @@
 import request from 'supertest'
 import { server } from '../../src/app'
 import { BlogInputModel } from '../../src/models/blogs/BlogInputModel'
+import { CommentInputModel } from '../../src/models/comments/CommentInputModel'
 import { PostInputModel } from '../../src/models/posts/PostInputModel'
 import { app, HTTP_STATUSES, baseURL } from '../../src/settings'
 
@@ -19,6 +20,31 @@ describe('/posts', () => {
         description: 'desc',
         websiteUrl: 'www.website.com'
     }
+
+    let recievedAccessToken = ''
+
+    const createUserInputData = {
+        login: 'login',
+        password: 'password',
+        email: 'email@email.ru'
+    }
+
+    it('should create user and should login, getting accessToken', async () => {
+        await request(app)
+            .post(`${baseURL}users`)
+            .set('Authorization', 'Basic YWRtaW46cXdlcnR5')
+            .send(createUserInputData)
+            .expect(HTTP_STATUSES.CREATED_201)
+
+        const accessTokenResponseData = await request(app)
+            .post(`${baseURL}auth/login`)
+            .send({
+                loginOrEmail: createUserInputData.login,
+                password: createUserInputData.password
+            })
+            .expect(HTTP_STATUSES.OK_200)
+        recievedAccessToken = accessTokenResponseData.body.accessToken
+    })
 
     let blogId = ''
     it('should create blog to get blogId ', async () => {
@@ -84,5 +110,34 @@ describe('/posts', () => {
         expect(updatedPostData.body.title).toEqual(correctUpdatePostBody.title)
         expect(updatedPostData.body.shortDescription).toEqual(correctUpdatePostBody.shortDescription)
         expect(updatedPostData.body.content).toEqual(correctUpdatePostBody.content)
+    })
+
+    const correctCommentBody: CommentInputModel = {
+        content: 'this content will over 20 symbols' // min 20 max 300
+    }
+    it('should create comment for current post and display correct info', async () => {
+        await request(app)
+            .post(`${baseURL}posts/${createdPostId}/comments`)
+            .set('Authorization', `Bearer ${recievedAccessToken}`)
+            .send(correctCommentBody)
+            .expect(HTTP_STATUSES.CREATED_201)
+
+        const recievedCreatedCommentsData = await request(app)
+            .get(`${baseURL}posts/${createdPostId}/comments`)
+            .expect(HTTP_STATUSES.OK_200)
+
+        expect(recievedCreatedCommentsData.body.items.length).toEqual(1)
+        expect(recievedCreatedCommentsData.body.items[0].content).toEqual(correctCommentBody.content)
+    })
+
+    it('should delete post', async () => {
+        await request(app)
+            .delete(`${baseURL}posts/${createdPostId}`)
+            .set('Authorization', `Basic YWRtaW46cXdlcnR5`)
+            .expect(HTTP_STATUSES.NO_CONTENT_204)
+
+        await request(app)
+            .get(`${baseURL}posts/${createdPostId}`)
+            .expect(HTTP_STATUSES.NOT_FOUND_404)
     })
 })
